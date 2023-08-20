f0d0da43f201f8451862cdd00b5c98887315172d
natsu445 <140126019+natsu445@users.noreply.github.com>
2023-08-21 00:40:56 +0800

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,15 @@
+//手機板導覽列
+
+
+
+window.addEventListener("scroll", function() {
+    const phone_nav = document.querySelector(".phone_nav");
+    // const contentHeight = document.querySelector(".content").clientHeight;
+  
+    if (window.scrollY > 0) {
+        phone_nav.classList.add("transparent");
+    } else {
+        phone_nav.classList.remove("transparent");
+    }
+  });
+  
